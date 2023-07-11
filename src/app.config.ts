94248869a55447f104273a1d85c90d21862f4179
fantasyLh <m17671430910@163.com>
2023-07-11 15:01:09 +0800

    init: 初始化项目

---
+++ src/app.config.ts
@@ -0,0 +1,10 @@
+import { pages } from './routes';
+export default defineAppConfig({
+	pages: pages,
+	window: {
+		backgroundTextStyle: 'light',
+		navigationBarBackgroundColor: '#fff',
+		navigationBarTitleText: 'WeChat',
+		navigationBarTextStyle: 'black'
+	}
+});
